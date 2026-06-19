d6a6789b1d3dfdadf2945227d8c76efc4a8a3255
Weverton Gabriel <120788852+WevertonGab@users.noreply.github.com>
2026-06-19 12:44:52 -0300

    Add files via upload

---
+++ jogo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylegame.css">
+</head>
+<body>
+    <section id="telajogo">
+        
+        <div id="hud">
+            <div id="bar"> 
+                <div id="tempo" class="icons"></div>
+                <button id="botao" class="icons" onclick="quebrabloco()">encerrar</button>
+                <div id="pontos" class="icons"></div>
+            </div>
+            <div id="game">
+                <div id="direita" class="fundo"></div>
+                <div id="jogo">
+                    <div id="player">
+                        <img id="player-sprite" src="imagens/sprite1.png" alt="player">
+                    </div>
+                    <div id="areablocos"></div>
+                </div>
+                <div id="esquerda" class="fundo"></div>
+            </div>
+        </div>
+    </section>
+    <script src="game.js"></script>
+</body>
+</html>
