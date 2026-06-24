c8e743506a7cf833fedde53d55327513a316de10
Weverton Gabriel <wevertonn581@gmail.com>
2026-06-24 00:35:54 -0300

    Adições no Banco de dados com relação as ligas

--- minigdb.sql
+++ minigdb.sql
@@ -34,4 +34,30 @@ entrou_em DATETIME,
 FOREIGN KEY (usuario_id) REFERENCES usuario(id),
 FOREIGN KEY (liga_id) REFERENCES ligas(id));
 
+/*rank Global Desde a Criação*/
+SELECT u.nickname, SUM(p.pontos) AS total_pontos
+FROM usuario u
+JOIN partida p ON p.usuario_id = u.id
+GROUP BY u.id
+ORDER BY total_pontos DESC;
+
+/*Rank Global semanal*/
+SELECT u.nickname, SUM(p.pontos) AS total_pontos
+FROM usuario u
+JOIN partida p ON p.usuario_id = u.id
+WHERE YEARWEEK(p.criado_em, 1) = YEARWEEK(NOW(), 1)
+GROUP BY u.id
+ORDER BY total_pontos DESC;
+
+/*Lista de Ligas*/
+SELECT l.id, l.nome, l.criado_em, u.nickname AS criador
+FROM ligas l
+JOIN usuario u ON u.id = l.criador_id
+ORDER BY l.criado_em DESC;
+
+/*Verificação se o usuário já está em outra liga*/
+SELECT liga_id FROM liga_membro WHERE usuario_id = ?;
+
 SELECT * FROM usuario;
+SELECT * FROM partida;
+SELECT * FROM ligas;
